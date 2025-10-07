9fc04272e6066e87d9c9ad168a5211727e6bc25c
Tan = Nintando <nintandot@gmail.com>
2025-10-07 15:37:27 +0200

    Exercice 2.2 - Paramétrage avec variables

--- exos/02_docker_nginx/variables.tf
+++ exos/02_docker_nginx/variables.tf
@@ -1,4 +1,4 @@
 variable "image" { default = "nginx:latest" }
-variable "container_name" { default = "mon-nginx" }
+variable "container_name" { default = "mmh_nginx" }
 variable "internal_port" { default = 80 }
 variable "external_port" { default = 8000 }
