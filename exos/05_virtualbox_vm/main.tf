ebeee48a196f7da35cb69f9503aadcd37b5ddeb5
Tan = Nintando <nintandot@gmail.com>
2025-10-07 17:38:49 +0200

    Exercice 5.1 – Créer une VM VirtualBox

---
+++ exos/05_virtualbox_vm/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
+    }
+  }
+}
+
+
+resource "virtualbox_vm" "vm1" {
+  name      = "ubuntu_vm"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "512 mib"
+  user_data = file("${path.module}/user_data")
+
+}
+
+
