50db9dd2e642ffe54ca7f3336f67e599b114d4ed
Tan = Nintando <nintandot@gmail.com>
2025-10-08 09:10:46 +0200

    Exercice 5.1 – Créer une VM VirtualBox

--- exos/05_virtualbox_vm/main.tf
+++ exos/05_virtualbox_vm/main.tf
@@ -13,7 +13,6 @@ resource "virtualbox_vm" "vm1" {
   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 1
   memory    = "512 mib"
-  user_data = file("${path.module}/user_data")
 
 }
 
